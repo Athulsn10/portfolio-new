0263f49287af223cedd56a4898d41ade5f7c8709
Athulsn10 <athulnair10@outlook.com>
2024-06-30 13:07:46 +0530

    landing page animation

--- src/components/LandingButton.tsx
+++ src/components/LandingButton.tsx
@@ -1,30 +1,44 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import './landingButton.css'; 
+import './landingButton.css';
 
 function LandingButton() {
   const navigate = useNavigate();
   const [animate, setAnimate] = useState(false);
-
+  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
+  const [showCircle, setShowCircle] = useState(true)
   const handleClick = () => {
     setAnimate(true);
     setTimeout(() => {
       navigator.vibrate(200);
       navigate('/home');
-    }, 400); 
+    }, 400);
   };
+  const handleShow = () => {
+    setShowCircle(!showCircle)
+  }
+  const handleMouseMove = (e) => {
+    setMousePosition({ x: e.clientX, y: e.clientY });
+  };
+  useEffect(() => {
 
+  })
   return (
-    <div className="d-flex w-100 vh-100  landing-btn-bg">
+    <div className="d-flex w-100 vh-100  landing-btn-bg" onMouseMove={handleMouseMove}>
       <div className='m-3 circle-container w-100 rounded d-flex align-items-center justify-content-center '>
-          <button onClick={handleClick} className="landing-button">
-            <span>Click Me</span>
-          </button>
-          {animate && (
-            <>
-              <div className="circle"></div>
-            </>
-          )}
+        <button onMouseEnter={handleShow} onMouseLeave={handleShow} onClick={handleClick} className="landing-button">
+          <span>Click Me</span>
+        </button>
+        {animate && (
+          <>
+            <div className="circle"></div>
+          </>
+        )}
+        {showCircle && (<div className='torch' style={{
+          top: mousePosition.y - 50,
+          left: mousePosition.x - 50
+        }}>
+        </div>)}
       </div>
     </div>
   );
