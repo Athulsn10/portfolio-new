1a278c686a3fa026bd96c014f8704ecf209b25e8
Athulsn10 <athulnair10@outlook.com>
2024-07-14 13:38:03 +0530

    updated about

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,14 +1,16 @@
 
 function About() {
   return (
-   <>
-   <div className="mt-3 ms-3">
-       <p className="guntertest-font hey">Hey!</p>
-       <p className="guntertest-font name" >Am, Athul S Nair</p>
-       <hr className="p-0 m-0 mb-3" />
-       <p className="fs-6 urbanist-light mb-4">Graduate of computer science. Intend to build a career with leading corporate of hi-tech environment with committed & dedicated people, which will help me to explore myself fully and realize my true potential.</p>
-   </div>
-   </>
+    <>
+      <div className="mt-3 ms-3 pe-3">
+        <p className="guntertest-font hey">Hey!</p>
+        <p className="guntertest-font name" >Am, Athul S Nair</p>
+        <hr className="p-0 m-0 mb-3" />
+        <p className="fs-6 urbanist-light mb-4 text-wrap">
+          A Software Engineer with a background in Computer Science from the University of Calicut and specialized training in the MERN stack. My journey in web development began with an enriching internship, where I developed and deployed web applications using MongoDB, Express, React.js, and Node.js. Currently, I apply my skills to create user-friendly and scalable web solutions, always aiming to tackle complex problems with innovative approaches.
+        </p>
+      </div>
+    </>
   )
 }
 
