f1e4398e91b7c3c4657b528c42b54eb01619abe7
Athulsn10 <athulnair10@outlook.com>
2025-09-09 19:16:59 +0530

    feat: integrate Vercel Analytics component into App

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import LandingButton from './page/LandingButton';
+import { Analytics } from "@vercel/analytics/react";
+
 function App() {
 
   return (
     <>
       <LandingButton />
+      <Analytics/>
     </>
   )
 }
